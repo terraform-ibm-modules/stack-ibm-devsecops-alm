55ecb9bbed8d25b07984e7e3e1bdeddf0c64e867
Padraic Edwards <padraic.edwards@ie.ibm.com>
2024-06-20 16:33:42 +0100

    chore: add readme

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,26 +2,27 @@ package tests
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testprojects"
 	"testing"
 )
 
 func TestProjectsFullTest(t *testing.T) {
+
 	options := testprojects.TestProjectOptionsDefault(&testprojects.TestProjectsOptions{
-		Testing: t,
-		Prefix:  "alm-stack",
-		StackConfigurationOrder: []string{
-			"1 - Infrastructure for Application Lifecycle Management",
-			"2 - Application Lifecycle Management"
-		},
+		Testing:        t,
+		ResourceGroup:  "default",
+		Prefix:         "alm", // setting prefix here gets a random string appended to it
+		ParallelDeploy: true,
 	})
 
 	options.StackInputs = map[string]interface{}{
-		"ibmcloud_api_key":            options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"],
 		"prefix":                      options.Prefix,
-		"region":                      options.Region
-		
+		"resource_group_name":         options.Prefix,
+		"sm_service_plan":             "trial",
+		"scc_service_plan":            "security-compliance-center-trial-plan",
+		"region":                      "us-south",
+		"use_existing_resource_group": false,
+		"ibmcloud_api_key":            options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], // always required by the stack
 	}
 
 	err := options.RunProjectsTest()
@@ -31,4 +32,3 @@ func TestProjectsFullTest(t *testing.T) {
 		t.Error("TestProjectsFullTest Failed")
 	}
 }
-
